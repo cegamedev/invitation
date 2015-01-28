ca0547e83e7958bd075bcb9f52e4c7858f33cf1d
spirit <spirit@spiritdeMacBook-Pro.local>
2015-01-28 08:15:03 +0800

    d

--- src/invitation/invitationScene.js
+++ src/invitation/invitationScene.js
@@ -2,9 +2,12 @@ var canChangeLayer = true;
 var curLayer = 0;
 var invitationScene = cc.Scene.extend({
 	ctor:function(){
-		
+		this._super();
 	},
 	onEnter:function(){
+		this._super();
+	},
+	initUI:function(){
 		
 	}
 });
